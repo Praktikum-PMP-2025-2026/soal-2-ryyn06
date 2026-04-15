a9166fe293536ce9461f3a37b61534896a591ee4
ryyn06 <rayyanhwr@gmail.com>
2026-04-15 10:01:49 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,5 +1,51 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : 1
+ *   Hari dan Tanggal    : Rabu, 15 April 2026
+ *   Nama (NIM)          : Achmad Rayyan Hawari
+ *   Nama File           : soal2.c
+ *   Deskripsi           : menghitung langkah sesuai ketentuan transformasi tertentu
+ * 
+ */
+
 #include <stdio.h>
 
-int main(void) {
-    return 0;
+int Totallangkah(int N) {
+    int langkah = 0 ;
+    while (N !=1)
+    {
+        
+        if (N%2 == 0)
+        {
+            N = N/2;
+            langkah ++;
+        }
+        else
+        {
+            N = 3*N + 1;
+            langkah++;
+        }
+    }
+    
+    return langkah;
 }
+
+int main(){
+    int N, angka[100];
+
+    scanf("%d", &N );
+    int i = 0;
+
+    for (size_t i = 0; i < N; i++)
+    {
+        scanf("%d", &angka[i]);
+    }
+    
+
+    for (size_t i = 0; i < N; i++)
+    {
+        printf("Langkah %d\n", Totallangkah(angka[i]));
+    }
+    
+    return 0 ;
+}
+
